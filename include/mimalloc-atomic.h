c64acec7c6bb506d1fb58173a73280efd54fe869
Ganesan Rajagopal <rganesan@gmail.com>
2022-12-12 15:38:03 +0530

    ATOMIC_VAR_INIT is deprecated in C17
    It's unneeded in C11, so make it a no-op.

--- include/mimalloc-atomic.h
+++ include/mimalloc-atomic.h
@@ -39,7 +39,7 @@ terms of the MIT license. A copy of the license can be found in the file
 #include <stdatomic.h>
 #define  mi_atomic(name)        atomic_##name
 #define  mi_memory_order(name)  memory_order_##name
-#define  MI_ATOMIC_VAR_INIT(x)  ATOMIC_VAR_INIT(x)
+#define  MI_ATOMIC_VAR_INIT(x)  x
 #endif
 
 // Various defines for all used memory orders in mimalloc
